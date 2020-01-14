cb7025b1cf97559dc33b34b48e46db027ea3cf96
yangb <523084685@qq.com>
2020-01-14 10:24:01 +0800

    Updates

--- help.sh
+++ help.sh
@@ -3,22 +3,16 @@ echo "
 2. 自动提交推送代码 [github 和 gitee]
 "
 
-read -p '> ' cmd
+gitbook build
+gh-pages -d _book
+gh-pages -d _book -o gitee
+echo 'http://yangb92.gitee.io/book
+https://yangb92.github.io/book/
+'
 
-case ${cmd} in
-    1) 
-        gitbook build
-        gh-pages -d _book
-        gh-pages -d _book -o gitee
-        echo 'http://yangb92.gitee.io/book
-        https://yangb92.github.io/book/
-        '
-    ;;
-    2) 
-        git add .
-        git commit -m '-> tool auto commit'
-        git push origin master
-        git push gitee master
-    ;;
-esac
+git add .
+git commit -m '-> tool auto commit'
+git push origin master
+git push gitee master
 
+read
